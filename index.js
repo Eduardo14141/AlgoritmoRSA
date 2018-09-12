ac92f2429db91d5f406f1759843d741fa0c22a5c
Eduardo14141 <edu_0601@live.com.mx>
2018-09-12 18:40:07 -0500

    inicio

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+var express = require('express');
+var body_parser = require('body-parser');
+
+var textos= function(req, res){
+
+}
+var sistema = http.createServer();
+sistema.listen(8080);
